c39871d8c17613db8de9b8108bc2f511aaedc904
Vyacheslav Sviridov <Vyacheslav.Sviridov@tele2.ru>
2019-08-02 10:06:10 +0600

    initial commit

---
+++ runWFScr.ps1
@@ -0,0 +1,47 @@
+#
+# powershell script for transmitting
+# command files into WinFIOL channels
+#
+
+$logfile = "powershell.log"
+
+$winfiol = New-Object -ComObject Ericsson.WinFIOL.1
+$winfiol.SetClientName("Power Shell Script")
+$winfiol.Show()
+$winfiol.KeepOpen()
+$winfiol.ShowStatusLine("Let's transmit!", 3000)
+
+$docpath = [environment]::getfolderpath("mydocuments")
+$CSVfile = "$docpath\powerShell\channels.csv"
+
+$colStats = Import-CSV $CSVfile
+
+foreach ($wfChannel in $colStats)
+	{
+		[ref] $n_cnannel = [int] $wfChannel.channel
+		$path = $wfChannel.path
+		$Chanfile = "$docpath\$path"
+		try{
+			$winfiol.OpenChannelFile($Chanfile, 0, $n_cnannel)
+		}
+		finally{
+			$channel = $winfiol.GetChannelPointer($n_cnannel.value)
+		}
+		$channel.TerminalConnect("", "")
+		Do{
+			$status = $channel.GetPortStatus()
+		}
+		Until($status -eq 2)
+		$channel.OutputLog(1, $logfile)
+		$channel.Transmit("allip.cmd", 1)
+		Do{
+			$status = $channel.GetBusyStatus()
+			#echo $status
+		}
+		Until($status -eq 2)
+		$channel.TerminalRelease()
+		$channel.OutputLog(0, "")
+	}
+
+
+#$winfiol.Close()
